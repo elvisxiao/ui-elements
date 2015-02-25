93c414ac2aff4f1ebfd69b36d27d7e07f8707a3a
Elvis <elvis.xiao@oceanwing.com>
2015-02-25 09:56:52 +0800

    save

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ process.env.NPM_CONFIG_PRODUCTION && (process.env.NPM_CONFIG_PRODUCTION = true);
 
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
 
-var server = app.listen(app.get('port'), process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1", function() {
+var server = app.listen(app.get('port'), process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0", function() {
    console.log('Express server listening on port ' + server.address().port);
 });
 
