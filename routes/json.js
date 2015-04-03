6cd00d193ac208c9cc9e51389f0833d1709f58bd
Elvis <elvis.xiao@oceanwing.com>
2015-04-03 13:02:04 +0800

    save

--- routes/json.js
+++ routes/json.js
@@ -5,20 +5,21 @@ var path = require('path');
 
 var readFile = function(req, res){
     console.log(req.params);
-    var fileName = req.params.file;
+    console.log(req.query);
+    var fileName = req.query.file;
     var filePath = path.join(path.dirname(__dirname), 'json', fileName);
     console.log(filePath);
     fs.readFile(filePath, 'utf-8', function(err, data){
-        res.send(JSON.stringify(data));
+    	res.send(req.query.callback + '(' + data + ')');
     })
 }
 
-router.get('/:file', readFile);
+router.get('/', readFile);
 
-router.post('/:file', readFile);
+router.post('/', readFile);
 
-router.put('/:file', readFile);
+router.put('/', readFile);
 
-router.delete('/:file', readFile);
+router.delete('/', readFile);
 
 module.exports = router;
