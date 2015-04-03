bd6dfb3d8ad26b47f5dbe1d5ce222ad4ddeb0043
Elvis <elvis.xiao@oceanwing.com>
2015-04-03 13:05:33 +0800

    save

--- routes/json.js
+++ routes/json.js
@@ -8,10 +8,18 @@ var readFile = function(req, res){
     console.log(req.query);
     var fileName = req.query.file;
     var filePath = path.join(path.dirname(__dirname), 'json', fileName);
-    console.log(filePath);
-    fs.readFile(filePath, 'utf-8', function(err, data){
-    	res.send(req.query.callback + '(' + data + ')');
-    })
+    var sendFile = function(){
+    	fs.readFile(filePath, 'utf-8', function(err, data){
+	    	res.send(req.query.callback + '(' + data + ')');
+	    })
+    }
+    if(req.query.timeout){
+    	setTimeout(sendFile, req.query.timeout);
+    }
+    else{
+    	sendFile();
+    }
+   
 }
 
 router.get('/', readFile);
