75b8e7b7911d60b4ae730938cad245b09ad0502b
Elvis <elvis.xiao@oceanwing.com>
2015-04-14 13:20:24 +0800

    save

--- routes/hook.js
+++ routes/hook.js
@@ -2,12 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/', function(req, res) {
-	console.log('commit code', req.body);
+	// console.log('commit code', req.body);
 	var exec = require('child_process').exec;
 	var cmdStr = 'git pull';
 	exec(cmdStr, function(err, stdout, stderr){
 		console.log(stdout);
 	})
+	res.send('hook called');
 });
 
 
