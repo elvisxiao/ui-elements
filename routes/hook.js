21b7533e6763d8161b6dad3ea1eaa05cd18f677a
Elvis <elvis.xiao@oceanwing.com>
2015-06-03 14:08:52 +0800

    save

--- routes/hook.js
+++ routes/hook.js
@@ -10,7 +10,6 @@ router.post('/', function(req, res) {
 	}
 
 	try{
-<<<<<<< HEAD
 		var repoName = '';
 		if(req.body.repository && req.body.repository.name){
 			repoName = req.body.repository.name
@@ -19,12 +18,6 @@ router.post('/', function(req, res) {
 			console.log(JSON.parse(req.body.payload));
 			repoName = JSON.parse(req.body.payload).repository.name;
 		}
-		
-=======
-		console.log(req.body);
-		
-		var repoName = JSON.parse(req.body.payload).repository.name;
->>>>>>> 657d80d2381803d58d44bed1b8c551f184963bf2
 		console.log('repo name:' + repoName);
 		var exec = require('child_process').exec;
 		var cmdStr = 'cd ~/bitbucket/' + pathMap[repoName] + ' && git pull';
