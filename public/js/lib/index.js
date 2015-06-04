f9f7ae0651b3148ba38c34adcb5bd101a8ce6810
Elvis <elvis.xiao@oceanwing.com>
2015-06-04 13:51:05 +0800

    save

--- public/js/lib/index.js
+++ public/js/lib/index.js
@@ -24,7 +24,7 @@
 		cssPath = cssPath.replace('oc.css', 'icons/style.css');
 		$("<link>").attr({ rel: "stylesheet", type: "text/css", href: cssPath}).appendTo("head");
 	}
-	else if(location.url.indexOf('tinyp2p') > -1){
+	else if(location.url.indexOf('tinyp2p') > -1 || location.url.indexOf('local') > -1){
 		$("<link>").attr({ rel: "stylesheet", type: "text/css", href: 'http://ui.tinyp2p.com/dest/oc.css'}).appendTo("head");
 		$("<link>").attr({ rel: "stylesheet", type: "text/css", href: 'http://ui.tinyp2p.com/icons/style.css'}).appendTo("head");
 	}
