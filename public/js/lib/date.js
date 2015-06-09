2a00b1c66bcd9d3af5d84aa4bf75f77bcff8683b
Elvis <elvis.xiao@oceanwing.com>
2015-06-09 17:44:30 +0800

    save

--- public/js/lib/date.js
+++ public/js/lib/date.js
@@ -25,7 +25,7 @@ var ZDate = {};
 * @returns {string} 格式化后的字符串
 */
 ZDate.format = function(date, format){
-	if(date.toString().indexOf('-') > 0){
+    if(date.toString().indexOf('-') > 0){
         date = date.toString().replace(/-/g, '/');
     }
 
@@ -59,7 +59,7 @@ ZDate.format = function(date, format){
         var param = reg[key];
         var val = model[param];
         if(val.toString().length < 2){
-        	val = '0' + val.toString();
+            val = '0' + val.toString();
         }
         format = format.replace(key, val);
     }
@@ -85,6 +85,150 @@ ZDate.compare = function(date1, date2){
     var date2 = new Date(date2).getTime();
 
     return date1 - date2;
+}   
+
+/**
+* 根据传入的年，获取改年一共有多少周
+* @param {nummber} year 四位的年（2015）
+* @returns {nummber} 这一年一共有多少周，（52/53）
+*/
+ZDate.getWeeksByYear = function(year){
+    var ret = 52;
+
+    var year = parseInt(year + "");
+    
+    var has53Years = [1994,2000,2005,2011,2016,2022,2028,2033,2039,2044,2050,2056,2061,2067,2072,2078,2084,2089,2095,2101,2107,2112,2118,2124,2129,2135,2140,2146,2152,2157,2163,2168,2174,2180,2185,2191,2196,2203,2208,2214,2220,2225,2231,2236,2242,2248,2253,2259,2264,2270,2276,2281,2287,2292,2298,2304,2310,2316,2321,2327,2332,2338,2344,2349,2355,2360,2366,2372,2377,2383,2388,2394];
+    
+    if($.inArray(year, has53Years) !== -1){
+        ret ++;
+    }
+
+    return ret;
+}
+
+/**
+* 根据传入的date字符串或者timespan，返回该天在这一年中的第几周中：201406 - 2015年06周
+* @param {object} date 传入的date字符串（2014-12-12或者2014/12/12)，或者timespan，默认值为JS当天
+* @returns {string} 该天在这一年中的第几周中，如：201406 - 2015年06周
+*/
+ZDate.getWeekString = function(date) {
+    if (!date) {
+        date = new Date();
+    }
+    if (typeof date === 'string') {
+        date = date.replace(/-/g, '/');
+    }
+    date = new Date(date);
+
+    if (date.getMonth() == 11 && date.getDate() > 20) {
+        var anotherDay = new Date(date);
+        anotherDay.setDate(anotherDay.getDate() + 6 - anotherDay.getDay());
+
+        if (anotherDay.getFullYear() > date.getFullYear()) {
+            return anotherDay.getFullYear() * 100 + 1;
+        }
+    }
+    var firstDayOfYear = new Date(date.getFullYear(), 0, 1);
+    var day = firstDayOfYear.getDay();
+    firstDayOfYear.setDate(8 - firstDayOfYear.getDay());
+
+    if (firstDayOfYear > date) {
+        return date.getFullYear() * 100 + 1;
+    }
+    var week = Math.floor((date - firstDayOfYear) / (1000 * 60 * 60 * 24 * 7));
+
+    return date.getFullYear() * 100 + week + 2;
+}
+
+ZDate.weekPicker = function(ipt){
+    var ipt = $(ipt);
+    var initVal = $.trim(ipt.val());
+
+    var getEle = function(){
+        var table = $('<table class="zWeekPicker"></table>');
+        var thead = $('<thead></thead>').appendTo(table);
+        var curr = new Date();
+        if(!initVal){
+            initVal = ZDate.getWeekString(curr).toString();
+        }
+        year = initVal.slice(0, 4);
+        week = initVal.slice(4);
+        
+        thead.append('<tr><th colspan="100"><i class="icon-arrow-left"></i><span class="spanYear">' + year + '</span><i class="icon-arrow-right"></i></th></tr>');
+
+        var tbody = $('<tbody></tbody>').appendTo(table);
+        var year = thead.find('.spanYear').text();
+
+        var weekCount = ZDate.getWeeksByYear(year);
+
+        var tr = $('<tr></tr>').appendTo(tbody);
+        for(var i = 1; i <= weekCount; i++){
+            if(i % 7 === 0){
+                tr = $('<tr></tr>').appendTo(tbody);
+            }
+            var str = i.toString();
+            if(i < 10){
+                str = '0' + i;
+            }
+            tr.append('<td>' + str + '</td>')
+        }
+
+        tbody.find('td:contains(' + week + ')').addClass('active');
+
+        table.on('click', 'thead i', function(){
+            var i = $(this);
+            var eleYear = i.parent().find('.spanYear');
+            var year = parseInt(eleYear.text());
+            if(i.hasClass('icon-arrow-left')){
+                year --;
+            }
+            else{
+                year ++;
+            }
+            weekCount = ZDate.getWeeksByYear(year);
+            console.log(weekCount);
+            if(weekCount == 52){
+                table.find('td:contains(53)').remove();
+            }
+            else if(table.find('td:contains(53)').length === 0){
+                table.find('tbody tr:last-child').append('<td>53</td>');
+            }
+            eleYear.html(year);
+        })
+        .on('click', 'tbody td', function(){
+            table.hide();
+            var year = table.find('.spanYear').text();
+            var week = $(this).html();
+            var text = year + week
+            ipt.val(text);
+        })
+
+        return table;
+    }
+
+    ipt = $(ipt);
+    ipt.on('click', function(){
+        var ele = $('.zWeekPicker');
+        if(ele.length === 0){
+            ele = getEle();
+            ele.appendTo('body');
+        }
+        var position = ipt.position();
+        var val = $.trim(ipt.val());
+        if(val && val.length === 6){
+            var year = val.slice(0, 4);
+            var week = val.slice(4);
+
+            ele.find('.spanYear').html(year);
+            ele.find('td.active').removeClass('active');
+            ele.find('td:contains(' + week + ')').addClass('active');
+        }
+        ele.css({
+            'left': position.left,
+            'top': position.top + ipt.outerHeight(),
+            'display': 'block'
+        })
+    })
 }   
 
 
